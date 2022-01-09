e3b73293a307393a02d2f2014cc2218e0ccc90d5
Raffaele <f4ff0@icloud.com>
2022-01-09 12:46:46 +0100

    Initial Commit

---
+++ Statistica/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Statistica
+//
+//  Created by Inartespacco on 09/01/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
